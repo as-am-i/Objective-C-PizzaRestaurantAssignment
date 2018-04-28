985d00e888fdfc4b853db1f95dc681303d6e32b2
Asami Tanii <g033.9521@gmail.com>
2018-04-27 17:35:38 -0700

    fixed issues for part2

--- PizzaRestaurant/main.m
+++ PizzaRestaurant/main.m
@@ -51,7 +51,6 @@ int main(int argc, const char * argv[])
             if ([commandWords count] == 2 && [first isEqualToString:@"call"]) {
                 if ([second isEqualToString:@"Asami"] || [second isEqualToString:@"asami"]) {
                     [restaurantKitchen setDelegate:anchoviesHater];
-                    NSLog(@"Asami called");
                     continue;
                 } else if ([second isEqualToString:@"Jesus"] || [second isEqualToString:@"jesus"]) {
                     [restaurantKitchen setDelegate:pizzaLover];
@@ -77,6 +76,11 @@ int main(int argc, const char * argv[])
                 NSLog(@"Enjoy 🍾 with pizza!");
                 continue;
             } else {
+                // when user haven't called any manager
+                if (restaurantKitchen.delegate == nil) {
+                    [restaurantKitchen setDelegate:normalManager];
+                }
+                
                 size = [restaurantKitchen getSizeOfPizza:first];
                 
                 NSMutableArray *mutableCommandWords = [NSMutableArray arrayWithArray:commandWords];
